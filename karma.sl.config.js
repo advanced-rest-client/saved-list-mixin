668e6354eaf1821359d2b6ae4885a6de8894e5fc
Pawel <jarrodek@gmail.com>
2020-03-30 20:51:50 -0700

    test: updating SL configuration

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -1,32 +1,31 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const merge = require('deepmerge');
-const slSettings = require('@advanced-rest-client/testing-karma-sl/sl-settings.js');
+const { slSettings } = require('@advanced-rest-client/testing-karma-sl');
 const createBaseConfig = require('./karma.conf.js');
 
 module.exports = (config) => {
-  const cnf = slSettings();
-  cnf.sauceLabs.testName = 'saved-list-mixin';
-  cnf.browsers = [
-    'SL_Chrome',
-    'SL_Chrome-1',
-    'SL_Firefox',
-    'SL_Firefox-1',
-    'SL_Safari',
-    // 'SL_EDGE'
-  ];
-  if (process.env.TRAVIS) {
-    const buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
+  const slConfig = merge(slSettings(config), {
+    sauceLabs: {
+      testName: 'saved-list-mixin',
+    },
+    client: {
+      mocha: {
+        timeout: 10000
+      }
+    },
 
-    cnf.browserStack = {
-      build: buildLabel,
-      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
-    };
+    /*
+    browsers: [
+      'SL_Chrome',
+      'SL_Chrome-1',
+      'SL_Firefox',
+      'SL_Firefox-1',
+      'SL_Safari',
+      // 'SL_EDGE'
+    ]
 
-    cnf.sauceLabs.build = buildLabel;
-    cnf.sauceLabs.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
-  }
-
-  config.set(merge(createBaseConfig(config), cnf));
-
+     */
+  });
+  config.set(merge(createBaseConfig(config), slConfig));
   return config;
 };
